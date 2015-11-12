bc6306b9139a3b3d1a14f5ae0a1e251f4969bcd7
Carlo Alberto Degli Atti <lordkada@gmail.com>
2015-11-12 10:46:41 +0100

    cleaning and reorganizing init code. Propedeutic to #15

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,11 +5,9 @@ var email_element = contact_section_element.find("[name='email']");
 var message_element = contact_section_element.find("[name='message']");
 var submit_button_element = contact_section_element.find("[data-id='contact-submit']");
 var calculation_label_element = contact_section_element.find("[data-id='calculation-label']");
-var first_number = 0;
-var second_number = 0;
-var op_valid = null;
 
-var validate = function() {
+
+var validate = function(value) {
     contact_form_element.validate( {
         messages: {
             "name": {
@@ -22,7 +20,7 @@ var validate = function() {
                             required: 'Inserisci il risultato dell\'operazione'}
         },
         rules : {
-            calculation : { calculation : true }
+            calculation : { equal: value }
         }
     });
 }
@@ -31,33 +29,15 @@ var confirm_form = function() {
     submit_button_element.replaceWith("<span>Ok, grazie per averci contattato!</span>");
 }
 
-generate_random_operation = function() {
-    var getRandomInt = function(min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    first_number = getRandomInt(1, 10);
-    second_number = getRandomInt(1, 10);
-    op = getRandomInt(0,2);
-    var math_op = first_number + ((op==0)? " * " : " + ") + second_number;
-    console.log(eval(math_op));
-    calculation_label_element.html("Inserisci il risultato dell'operazione " + math_op.replace("*", "x") + " = ");
-    op_valid = function(val) {
-        return val == eval(math_op);
-    }
-}
-
 
 $(document).ready(function() {
-    jQuery.validator.addMethod("calculation", function(value, element, params) {
-        return op_valid(value);
-    }, jQuery.validator.format("Inserisci il risultato corretto"));
+   var operation_checker = window.generate_random_operation();
+   calculation_label_element.html( "Inserisci il risultato dell'operazione " + operation_checker.operation + " = " );
 
-    generate_random_operation();
-
-    contact_form_element.submit( function(e) {
+   contact_form_element.submit( function(e) {
         e.preventDefault();
-        validate();
+
+        validate(operation_checker.result);
 
         if (contact_form_element.valid()) {
             submit_button_element.attr("disabled", "disabled");
---
+++ assets/js/init.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+
+    // Validation form initialisation
+    jQuery.validator.addMethod("equal", function(value, element, param) {
+            return value == param;
+        }, jQuery.validator.format("Inserisci il risultato corretto"));
+
+    window.generate_random_operation = function() {
+        var getRandomInt = function(min, max) {
+          return Math.floor(Math.random() * (max - min)) + min;
+        }
+
+        var math_op = getRandomInt(1, 10) + ((getRandomInt(0,2)==0)? " * " : " + ") + getRandomInt(1, 10);
+
+        return {
+            result: eval(math_op),
+            operation: math_op.replace("*", "x")
+        }
+    }
+
+    // jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+})
