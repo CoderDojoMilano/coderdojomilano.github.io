1bf96217c9737975f031ee3a1b3d088ca7bc837b
Giampiero Baggiani <giampiero7@users.noreply.github.com>
2016-10-12 22:16:55 +0200

    Update locations.js

--- _layouts/locations.js
+++ _layouts/locations.js
@@ -1,2 +1,2 @@
 var event_locations = [{% for location in site.event_locations %}
-  {did: "{{location.id}}", name:"{{location.name}}", lat:{{location.lat}}, lon:{{location.lon}}, url:"{{location.url}}", logo:"{{location.logo}}", addr:"{{location.addr}}"},{% endfor %}];
+  {did: "{{location.id}}", name:"{{location.name}}", lat:{{location.lat}}, lon:{{location.lon}}, url:"{{location.url}}", logo:"{{location.logo}}", addr:"{{location.addr}}, no_home_map:"{{location.no_home_map}}"},{% endfor %}];
