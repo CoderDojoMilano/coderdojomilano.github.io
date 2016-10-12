b59414d0bbda9be2e21908decf97279fe100b474
Giampiero Baggiani <giampiero7@users.noreply.github.com>
2016-10-12 22:39:48 +0200

    Update locations.js

--- _layouts/locations.js
+++ _layouts/locations.js
@@ -2,7 +2,7 @@ var event_locations = [
 {% for location in site.event_locations %}
   {
   {% for attr in location %}
-    {{attr[0]}}:{{attr[1]}},
+    {{ attr[0] }}:"{{ attr[1] }}",
   {% endfor %}
   }
 {% endfor %}
